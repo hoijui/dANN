eaf4a545db73457130c4605f1476d16465b28050
chickenf <>
2006-06-04 18:39:16 +0000

    initial version - structure only
    git-svn-id: svn://svn.syncleus.com/dANN/trunk@43 6ae8b97b-f314-0410-8212-aecf10b92ded

---
+++ source/java_dann_examples/java/com/syncleus/core/dann/examples/nci/ui/MainWindow.java
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *                                                                             *
+ *  Copyright: (c) Jeffrey Phillips Freeman                                    *
+ *                                                                             *
+ *  You may redistribute and modify this source code under the terms and       *
+ *  conditions of the Open Source Community License - Type C version 1.0       *
+ *  or any later version as published by syncleus at http://www.syncleus.com.  *
+ *  There should be a copy of the license included with this file. If a copy   *
+ *  of the license is not included you are granted no right to distribute or   *
+ *  otherwise use this file except through a legal and valid license. You      *
+ *  should also contact syncleus at the information below if you cannot find   *
+ *  a license:                                                                 *
+ *                                                                             *
+ *  Syncleus                                                                   *
+ *  1116 McClellan St.                                                         *
+ *  Philadelphia, PA 19148                                                     *
+ *                                                                             *
+ ******************************************************************************/
+
+
+package com.syncleus.core.dann.examples.nci.ui;
+
+import javax.swing.JFrame;
+
+
+/**
+ * A graphical user interface based on java swing
+ * for the nci example application of dANN.
+ * <!-- Author: chickenf -->
+ * @author chickenf
+ */
+
+public class MainWindow extends JFrame {
+
+	private int foo;
+
+	public MainWindow() {
+		setTitle("GUI test");
+	}
+	
+	public static void main(String args[]) {
+		MainWindow myMainwindow = new MainWindow();
+	}
+	
+}
