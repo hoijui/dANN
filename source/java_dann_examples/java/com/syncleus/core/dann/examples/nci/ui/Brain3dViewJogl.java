bdfbad964ea81f8d9ccc7159ec189a1f1e30f66d
chickenf <>
2006-06-14 03:00:51 +0000

    Basic encapsulation of the 3d scene in a Sing JFrame.
    git-svn-id: svn://svn.syncleus.com/dANN/trunk@73 6ae8b97b-f314-0410-8212-aecf10b92ded

--- source/java_dann_examples/java/com/syncleus/core/dann/examples/nci/ui/Brain3dViewJogl.java
+++ source/java_dann_examples/java/com/syncleus/core/dann/examples/nci/ui/Brain3dViewJogl.java
@@ -35,11 +35,65 @@ package com.syncleus.core.dann.examples.nci.ui;
  */
 
 
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLDrawableFactory;
+import javax.media.opengl.GLCanvas;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Brain3dViewJogl extends JFrame {
 
+	////////////////
+	// CONFIG START
+	
+	// Size of the Window containing the 3d visualization
+	private final int JFRAME_WIDTH = 500;
+	private final int JFRAME_HEIGHT = 400;
+	private JFrame myMainWindow;
+
+	// CONFIG END
+	////////////////
+
 	public Brain3dViewJogl(JFrame myMainWindow) {
 		
+		this.myMainWindow = myMainWindow;
+
+		// initialization: creation of a GLCanvas with JOGL
+		GLCapabilities capabilities = new GLCapabilities();
+		GLCanvas myGLCanvas = GLDrawableFactory.getFactory().createGLCanvas(capabilities);
+
+//		myGLCanvas.addGLEventListener(this);
+	
+		myGLCanvas.setSize(JFRAME_WIDTH, JFRAME_HEIGHT);
+		myGLCanvas.setLocation(0, 0);
+
+	    // Create the canvas container.
+	    JPanel myPanel = new JPanel();
+	    myPanel.setSize(JFRAME_WIDTH, JFRAME_HEIGHT);
+	    myPanel.setLocation(0, 0);
+	    myPanel.setVisible(true);
+	    myPanel.setLayout(null);
+
+	    // Add the 3D canvas to the container.
+	    myPanel.add(myGLCanvas);
+
+	    // a the panel containing the 3D canvas to the JFrame
+	    getContentPane().add(myPanel);
+
+	    // Turn off the layout manager, widgets will be sized
+	    // and positioned explicitly.
+	    setLayout(null);
+	    setSize(JFRAME_WIDTH, JFRAME_HEIGHT);
+	    setTitle("dANN Brain visulization (JOGL)");
+
+	    // fix the window size for now...
+		setResizable(false);
+		
+		this.setLocationRelativeTo(myMainWindow); // will center the window according to the position of the MainWindow
+
+		// move the window a little
+		this.setLocation(this.getX() - 190, this.getY() - 100);
+		
+	    setVisible(true);
 	}
 }
