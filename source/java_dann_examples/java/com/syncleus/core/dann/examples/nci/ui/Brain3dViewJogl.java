1bf5bc23ac5b3b9abce3020b52e636f1d17766a5
chickenf <>
2006-06-14 02:26:02 +0000

    Initial template for the jogl version of the brain 3d view. The Java3D and JOGL API will both be tested for our purpose, the dANN brain 3d visualization.
    git-svn-id: svn://svn.syncleus.com/dANN/trunk@72 6ae8b97b-f314-0410-8212-aecf10b92ded

---
+++ source/java_dann_examples/java/com/syncleus/core/dann/examples/nci/ui/Brain3dViewJogl.java
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *                                                                             *
+ *  Copyright: (c)                                    *
+ *                                                                             *
+ *  You may redistribute and modify this source code under the terms and       *
+ *  conditions of the Open Source Community License - Type C version 1.0       *
+ *  or any later version as published by syncleus at http://www.syncleus.com.  *
+ *  There should be a copy of the license included with this file. If a copy   *
+ *  of the license is not included you are granted no right to distribute or   *
+ *  otherwise use this file except through a legal and valid license. You      *
+ *  should also contact syncleus at the information below if you cannot find   *
+ *  a license:                                                                 *
+ *                                                                             *
+ *  Syncleus                                                                   *
+ *  1116 McClellan St.                                                         *
+ *  Philadelphia, PA 19148                                                     *
+ *                                                                             *
+ ******************************************************************************/
+
+package com.syncleus.core.dann.examples.nci.ui;
+
+/**
+ * 3d component of the graphical user interface based on java swing
+ * for the nci example application of dANN.
+ * -> "nci" stands for "Neural Compressed Image".
+ * 
+ * This class implements the jogl Window of the gui, to
+ * be compared with the java3d version implemented in
+ * parallel.
+ * The nciBrain is visualized in 3d in a JFrame started
+ * from the MainWindow class (main window of the gui)
+ * 
+ * <!-- Author: chickenf -->
+ * @author chickenf
+ */
+
+
+import javax.swing.JFrame;
+
+public class Brain3dViewJogl extends JFrame {
+
+	public Brain3dViewJogl(JFrame myMainWindow) {
+		
+	}
+}
