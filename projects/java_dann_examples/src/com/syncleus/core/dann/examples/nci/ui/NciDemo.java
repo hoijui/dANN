9e0222487fa506976a670a00370b66548f49fe4d
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2008-08-26 01:27:14 +0000

    Fixed some debugging code that wasnt suppose to have made it into svn.
    git-svn-id: svn://svn.syncleus.com/dANN/trunk@131 6ae8b97b-f314-0410-8212-aecf10b92ded

--- projects/java_dann_examples/src/com/syncleus/core/dann/examples/nci/ui/NciDemo.java
+++ projects/java_dann_examples/src/com/syncleus/core/dann/examples/nci/ui/NciDemo.java
@@ -304,13 +304,13 @@ private void aboutMenuItemMenuKeyPressed(javax.swing.event.MenuKeyEvent evt) {//
 }//GEN-LAST:event_aboutMenuItemMenuKeyPressed
 
 private void originalImageSelectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_originalImageSelectActionPerformed
-    this.originalImageText.setText("C:\\Documents and Settings\\All Users\\Documents\\My Pictures\\Sample Pictures\\In3.PNG");
-    this.originalImageLocation = new File("C:\\Documents and Settings\\All Users\\Documents\\My Pictures\\Sample Pictures\\In3.PNG");
+//    this.originalImageText.setText("C:\\Documents and Settings\\All Users\\Documents\\My Pictures\\Sample Pictures\\In3.PNG");
+//    this.originalImageLocation = new File("C:\\Documents and Settings\\All Users\\Documents\\My Pictures\\Sample Pictures\\In3.PNG");
 
-    this.refreshOriginalImage();
+//    this.refreshOriginalImage();
 
-    if (true)
-        return;
+//    if (true)
+//        return;
 
     JFileChooser chooser = new JFileChooser();
     FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG Images", "png");
@@ -328,34 +328,34 @@ private void originalImageSelectActionPerformed(java.awt.event.ActionEvent evt) 
 }//GEN-LAST:event_originalImageSelectActionPerformed
 
 private void trainingDirectorySelectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_trainingDirectorySelectActionPerformed
-    this.trainingDirectoryText.setText("C:\\Documents and Settings\\All Users\\Documents\\My Pictures\\Sample Pictures");
-    this.trainingDirectory = new File("C:\\Documents and Settings\\All Users\\Documents\\My Pictures\\Sample Pictures");
-    /*
+    //this.trainingDirectoryText.setText("C:\\Documents and Settings\\All Users\\Documents\\My Pictures\\Sample Pictures");
+    //this.trainingDirectory = new File("C:\\Documents and Settings\\All Users\\Documents\\My Pictures\\Sample Pictures");
+
     JFileChooser chooser = new JFileChooser();
     chooser.setFileSelectionMode(chooser.DIRECTORIES_ONLY);
     chooser.setMultiSelectionEnabled(false);
     chooser.setVisible(true);
     if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
     {
-    this.trainingDirectoryText.setText(chooser.getSelectedFile().getAbsolutePath());
-    this.trainingDirectory = chooser.getSelectedFile();
-     */
+        this.trainingDirectoryText.setText(chooser.getSelectedFile().getAbsolutePath());
+        this.trainingDirectory = chooser.getSelectedFile();
 
-    try
-    {
 
-        File[] trainingFiles = trainingDirectory.listFiles(new PngFileFilter());
-        this.brainRunner = new BrainRunner(this, trainingFiles, 0.8, BLOCK_WIDTH, BLOCK_HEIGHT, true);
-        this.brainRunnerThread = new Thread(this.brainRunner);
-        this.brainRunnerThread.start();
+        try
+        {
+
+            File[] trainingFiles = trainingDirectory.listFiles(new PngFileFilter());
+            this.brainRunner = new BrainRunner(this, trainingFiles, 0.8, BLOCK_WIDTH, BLOCK_HEIGHT, true);
+            this.brainRunnerThread = new Thread(this.brainRunner);
+            this.brainRunnerThread.start();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Danger will robinson, Danger: " + e);
+            e.printStackTrace();
+            return;
+        }
     }
-    catch (Exception e)
-    {
-        System.out.println("Danger will robinson, Danger: " + e);
-        e.printStackTrace();
-        return;
-    }
-//    }
 }//GEN-LAST:event_trainingDirectorySelectActionPerformed
 
 private void trainButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_trainButtonActionPerformed
