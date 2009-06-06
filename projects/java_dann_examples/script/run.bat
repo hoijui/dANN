85285b36218c6436c748a0b8f111cbcaa8a75676
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2009-06-06 17:38:00 +0000

    Added linux startup scripts as well as fixed the bat scripts.
    git-svn-id: svn://svn.syncleus.com/dANN/trunk@259 6ae8b97b-f314-0410-8212-aecf10b92ded

--- projects/java_dann_examples/script/run.bat
+++ projects/java_dann_examples/script/run.bat
@@ -1,1 +1,2 @@
-java -Xmx512m -classpath ..\build\jar\dann_examples.jar;..\..\java_dann\build\jar\dann.jar com.syncleus.core.dann.examples.Main
+java -Xmx320m -classpath ..\build\jar\dann_examples.jar;..\..\java_dann\build\jar\dann.jar;../lib/j3dcore.jar;../lib/vecmath.jar;../lib/j3dutils.jar com.syncleus.core.dann.examples.Main
+@PAUSE
---
+++ projects/java_dann_examples/script/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -Xmx320m -classpath ..\build\jar\dann_examples.jar:..\..\java_dann\build\jar\dann.jar:../lib/j3dcore.jar:../lib/vecmath.jar:../lib/j3dutils.jar com.syncleus.core.dann.examples.Main
