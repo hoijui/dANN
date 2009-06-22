9941218b4e5f3f52f4c4bdc3563f14c0844e494a
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2009-06-22 11:00:26 +0000

    Renamed the libraries to more generic names (removed the version from the name).
    git-svn-id: svn://svn.syncleus.com/dANN/trunk@309 6ae8b97b-f314-0410-8212-aecf10b92ded

--- projects/java_dann_examples/script/run-genetics.sh
+++ projects/java_dann_examples/script/run-genetics.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx128m -classpath ../build/jar/dann_examples.jar:../../java_dann/build/jar/dann.jar:../lib/freehep-java3d-2.0.jar:../lib/j3dcore.jar:../lib/vecmath.jar:../lib/j3dutils.jar com.syncleus.core.dann.examples.test.Test3d
+java -Xmx128m -classpath ../build/jar/dann_examples.jar:../../java_dann/build/jar/dann.jar:../lib/freehep-j3d.jar:../lib/j3dcore.jar:../lib/vecmath.jar:../lib/j3dutils.jar com.syncleus.core.dann.examples.test.Test3d
